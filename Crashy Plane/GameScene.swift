26c1c6e327383e1cea4cc55cc85615f6ec665a3a
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-09 13:35:21 +0300

    createSky() is added

--- Crashy Plane/GameScene.swift
+++ Crashy Plane/GameScene.swift
@@ -30,9 +30,27 @@ class GameScene: SKScene {
         player.run(runForever)
     }
     
+    func createSky() {
+        let topSky = SKSpriteNode(color: UIColor(hue: 0.55, saturation: 0.14, brightness: 0.97, alpha: 1), size: CGSize(width: frame.width, height: frame.height))
+        topSky.anchorPoint = CGPoint(x: 0.5, y: 1)
+        
+        let bottomSky = SKSpriteNode(color: UIColor(hue: 0.55, saturation: 0.16, brightness: 0.96, alpha: 1), size: CGSize(width: frame.width, height: frame.height * 0.33))
+        bottomSky.anchorPoint = CGPoint(x: 0.5, y: 1)
+        
+        topSky.position = CGPoint(x: frame.midX, y: frame.height)
+        bottomSky.position = CGPoint(x: frame.midX, y: bottomSky.frame.height)
+        
+        addChild(topSky)
+        addChild(bottomSky)
+        
+        bottomSky.zPosition = -40
+        topSky.zPosition = -40
+    }
+    
     //MARK: -
     override func didMove(to view: SKView) {
         createPlayer()
+        createSky()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
